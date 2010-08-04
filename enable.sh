28558a326b824b0bae9ff9e948afa84795b4d54f
Boštjan Škufca <bostjan@a2o.si>
2010-08-05 00:31:32 +0200

    Fixed warnings in enable.sh when not using bash (uses sh now)

--- enable.sh
+++ enable.sh
@@ -8,7 +8,7 @@
 
 ### Where is the library installed?
 LIBDIR=$1
-if [ "x$LIBDIR" == "x" ]; then
+if [ -z "$LIBDIR" ]; then
     echo "ERROR: No argument given, libdir expected"
     exit 1
 fi
@@ -33,7 +33,7 @@ fi
 
 
 ### Do the actual install
-if [ `grep -c "^$LIBDIR/snoopy.so"   /etc/ld.so.preload` == 0 ]; then
+if [ `grep -c "^$LIBDIR/snoopy.so" /etc/ld.so.preload` -eq 0 ]; then
     echo "$LIBDIR/snoopy.so" >> /etc/ld.so.preload
     echo "Snoopy enabled in /etc/ld.so.preload. Check syslog messages for output."
 else
